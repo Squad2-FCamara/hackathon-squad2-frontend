c60c8eec91ae8c8a419d4fc21717660f04ed7322
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-09 17:30:41 -0300

    Atualizando header para reaproveitamento no bootstrap.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,13 +3,17 @@ import style from "./Header.module.scss"
 
 function Header() {
     return (
-        <header className={style.header}>
-            <nav>
+        <header>
+            <nav className={style.header}>
+                <h2>LOGO</h2>
+                <input typeof="search" className={style.inputBox} placeholder="Pesquisar um sangue laranja por nome, habilidades.."></input>
                 <ul className={style.navList}>
                     <li>Home</li>
                     <li>Fórum</li>
                     <li>Dicas de estudos</li>
                 </ul>
+                <p>Notificação</p>
+                <p>Imagem perfil</p>
             </nav>
         </header>
     )
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,14 +3,14 @@ import style from "./Homepage.module.scss"
 
 function Homepage () {
     return (
-        <main>
+        <section>
             <p>O conteúdo desta página estará sendo definido em breve. Aguarde! </p>
 
             <div className={style.mainContainer}>
                 <h2>Aqui temos uma tela!</h2> 
             </div>
 
-        </main>
+        </section>
     )
 }
 

1504cfc84b5850f6833da263ba4ead71cfacfefc
Aline Avelino <aline.savelino@gmail.com>
2022-04-10 17:11:24 -0300

    estrutura sidebar homepage

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,19 +1,40 @@
 import { Card } from "react-bootstrap"
 import styles from "./styles.module.scss"
-import persona from '/user-example.jpg'
+import eduarda from '/eduarda.jpg'
+import jeronimo from '/jeronimo.jpg'
+import luiza from '/luiza.jpg'
+import marcelina from '/marcelina.jpg'
 
 export function Homepage() {
     return (
         <main className={styles.pageContainer}>
             <section className={styles.sidebarContainer}>
-                <h1>sidebar</h1>
                 <Card style={{ width: '18rem' }} className={styles.userContainer}>
-                    <Card.Img variant="top" src={persona} className={styles.photo} alt="Marcelina é uma mulher negra, tem os cabelos cacheados, está num ambiente externo usando óculos escuros e sorrindo." />
+                    <Card.Img variant="top" src={eduarda} className={styles.photo} alt="Eduarda é uma mulher negra, tem os cabelos cacheados, está num ambiente externo usando óculos escuros e sorrindo." />
+                    <Card.Title>Eduarda Mônica</Card.Title>
+                </Card>
+
+                <Card style={{ width: '18rem' }}>
                     <Card.Body>
-                        <Card.Title>Marcelina Silva</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">marcelina.silva@fcamara.com</Card.Subtitle>
+                        <Card.Title>Perguntas no Fórum</Card.Title>
                         <Card.Text>
-                            Brasília/DF - Brasil
+                            Feitas 50 <br />
+                            Respondidas 48
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>Perfis Salvos</Card.Title>
+                        <Card.Text>
+                            <Card.Img style={{ width: '2.5rem', height: '2.5rem' }} variant="top" src={marcelina} className={styles.photo} alt="Marcelina é uma mulher branca, tem os cabelos ondulados, está num ambiente interno e sorrindo." /> Marcelina Silva
+                        </Card.Text>
+                        <Card.Text>
+                            <Card.Img style={{ width: '2.5rem', height: '2.5rem' }} variant="top" src={luiza} className={styles.photo} alt="Luíza é uma mulher branca, tem os cabelos liso, está num ambiente externo usando batom vermelho e sorrindo." /> Luíza Fernanda
+                        </Card.Text>
+                        <Card.Text>
+                            <Card.Img style={{ width: '2.5rem', height: '2.5rem' }} variant="top" src={jeronimo} className={styles.photo} alt="Jerônimo é um homem negro, tem barba, está sério, num ambiente externo e usando um chapeu." /> Jerônimo Cristino
                         </Card.Text>
                     </Card.Body>
                 </Card>

383e84d31f0611f7c147627363bea15f61a2de42
Aline Avelino <aline.savelino@gmail.com>
2022-04-10 03:36:38 -0300

    substituição componente FriendProfile por Homepage

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -1,13 +1,27 @@
+import { Card } from "react-bootstrap"
 import styles from "./styles.module.scss"
+import persona from '/user-example.jpg'
 
 export function Homepage() {
     return (
-        <section>
-            <p>O conteúdo desta página estará sendo definido em breve. Aguarde! </p>
+        <main className={styles.pageContainer}>
+            <section className={styles.sidebarContainer}>
+                <h1>sidebar</h1>
+                <Card style={{ width: '18rem' }} className={styles.userContainer}>
+                    <Card.Img variant="top" src={persona} className={styles.photo} alt="Marcelina é uma mulher negra, tem os cabelos cacheados, está num ambiente externo usando óculos escuros e sorrindo." />
+                    <Card.Body>
+                        <Card.Title>Marcelina Silva</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">marcelina.silva@fcamara.com</Card.Subtitle>
+                        <Card.Text>
+                            Brasília/DF - Brasil
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </section>
 
-            <div className={styles.mainContainer}>
-                <h2>Aqui temos uma tela!</h2>
-            </div>
-        </section>
+            <section className={styles.contentContainer}>
+                <h1>content</h1>
+            </section>
+        </main >
     )
 }

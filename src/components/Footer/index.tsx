ca69da7bec6af9882b32ed29a5cfec65ede0f581
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-10 10:59:26 -0300

    Deletando componente Footer.

--- src/components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss";
-
-export function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <h2>Este é o footer!</h2>
-        </footer>
-    )
-}

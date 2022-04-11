c0a2c2e5d2819f243081cc48ce953aa1c909d058
Thais Silveira <88152671+thaissilvr@users.noreply.github.com>
2022-04-11 14:50:51 -0300

    Fixando o header no topo da tela. Mudando px para rem.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { SyntheticEvent, useContext } from "react"
-import { Nav, Navbar, Container, Image, Form, FormControl } from "react-bootstrap"
+import { Nav, Navbar, Image, Form, FormControl } from "react-bootstrap"
 import { NavLink, useNavigate } from "react-router-dom"
 import { SearchContextType, SearchContext } from "../../context/SearchContext"
 import styles from "./styles.module.scss"
@@ -16,29 +16,23 @@ export function Header() {
     return (
         <header className={styles.header}>
             <Navbar collapseOnSelect expand="lg" >
-                <Container>
-                    <NavLink to={"/"}>LOGO</NavLink>
-
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Form onSubmit={handleSubmit}>
-                            <FormControl
-                                type="search"
-                                placeholder="search"
-                                className={styles.inputBox}
-                                aria-label="Search"
-                                onChange={handleInput}
-                            />
-                        </Form>
-                        <Nav className={styles.navList}>
-                            <NavLink to={"/"}>Home</NavLink>
-                            <NavLink to={"/"}>Fórum</NavLink>
-                            <NavLink to={"/"}>Dicas de estudos</NavLink>
-                            <NavLink to={"/"}>Icon</NavLink>
-                            <Image src={persona} className={styles.photo} alt="Eduarda é uma mulher negra, tem os cabelos cacheados, está num ambiente externo usando óculos escuros e sorrindo." />
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
+                <NavLink to={"/"}>LOGO</NavLink>
+                <Form onSubmit={handleSubmit}>
+                    <FormControl
+                        type="search"
+                        placeholder="search"
+                        className={styles.inputBox}
+                        aria-label="Search"
+                        onChange={handleInput}
+                    />
+                </Form>
+                <Nav className={styles.navList}>
+                    <NavLink to={"/"}>Home</NavLink>
+                    <NavLink to={"/"}>Fórum</NavLink>
+                    <NavLink to={"/"}>Dicas de estudos</NavLink>
+                    <NavLink to={"/"}>Icon</NavLink>
+                    <Image src={persona} className={styles.photo} alt="Eduarda é uma mulher negra, tem os cabelos cacheados, está num ambiente externo usando óculos escuros e sorrindo." />
+                </Nav>
             </Navbar>
         </header>
     )

b604e3cd8e28211d90d257bc3294b82619f49a8c
Aline Avelino <aline.savelino@gmail.com>
2022-04-12 02:32:58 -0300

    renderização informações do agendamento usando get

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -1,26 +1,35 @@
 import { Card } from "react-bootstrap";
 import { useQuery } from "react-query";
 import api from "../../services/api";
-import styles from "./styles.module.scss"
-import luiza from '/luiza.jpg'
+import styles from "./styles.module.scss";
+import luiza from '/luiza.jpg';
 
 type Appointment = {
-    name: String,
-    email: String,
-    UserSchedule: [
-        {
-            schedule: {
-                day: String,
-                start_time: String,
-                end_time: String,
-                description: String
+    user: {
+        id: number,
+        name: string,
+    },
+    schedule: {
+        id: number,
+        day: string,
+        start_time: string,
+        end_time: string,
+        description: string,
+        UserSchedule: [
+            {
+                user: {
+                    id: number,
+                    name: string,
+                    email: string,
+                }
             }
-        }]
+        ]
+    }
 }
 
 export function Appointment() {
-    const { data, isFetching } = useQuery<Appointment[]>('agendamentos', async () => {
-        const response = await api.get('http://localhost:4000/users')
+    const { data, isFetching } = useQuery<Appointment[]>('appointments', async () => {
+        const response = await api.get('http://localhost:4000/user/schedule/2')
 
         return response.data;
     }, {
@@ -30,14 +39,19 @@ export function Appointment() {
     return (
         <section>
             {isFetching && <p>Carregando...</p>}
-            {data?.map(agendamento => {
+            {data?.map(appointment => {
                 return (
-                    <Card style={{ width: '100%' }} >
-                        <Card.Body>
-                            <Card.Title>30 Outubro 2021 | início 16:00 - término 16:30</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted"><Card.Img style={{ width: '2.5rem', height: '2.5rem' }} variant="top" src={luiza} className={styles.photo} alt="Luíza é uma mulher branca, tem os cabelos liso, está num ambiente externo usando batom vermelho e sorrindo." />{agendamento.name}</Card.Subtitle>
+                    <Card style={{ width: '100%' }} key={appointment.schedule.id}>
+                        <Card.Body >
+                            <Card.Title>{appointment.schedule.day} | início {appointment.schedule.start_time} - término {appointment.schedule.end_time}</Card.Title>
+                            <Card.Img style={{ width: '2.5rem', height: '2.5rem' }} variant="top" src={luiza} className={styles.photo} alt={appointment.user.name} />
+                            {appointment.schedule.UserSchedule?.map(schedule => {
+                                if (schedule.user.id != appointment.user.id) {
+                                    return schedule.user.name
+                                }
+                            })}
                             <Card.Text>
-                                {agendamento.email}
+                                {appointment.schedule.description}
                             </Card.Text>
                         </Card.Body>
                     </Card>

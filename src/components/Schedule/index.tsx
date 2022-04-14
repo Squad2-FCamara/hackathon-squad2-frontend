aa70b8f7022f0c14b8aba7fe27b36570004d05ec
Aline Avelino <aline.savelino@gmail.com>
2022-04-14 20:49:05 -0300

    estilização página agendamento

--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardGroup, Form, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Button, Card, CardGroup, Container, Form } from "react-bootstrap";
 import { AvailabilityFriendProfile } from "../AvailabilityFriendProfile";
 import { Calendar } from "../Calendar";
 import styles from "./styles.module.scss";
@@ -21,44 +21,72 @@ export function Schedule() {
                     <AvailabilityFriendProfile />
                 </CardGroup>
 
-                <h2 style={{ fontWeight: '700' }}>Qual é o assunto?</h2>
-                <Form.Control
-                    type="text"
-                    placeholder="Escreva aqui o assunto da mentoria"
-                    className={styles.formStyle}
-                />
+                <Form.Group className="mb-3" controlId="formText">
+                    <Form.Label style={{ fontWeight: '700' }}>
+                        Qual é o assunto?
+                    </Form.Label>
+
+                    <Form.Control
+                        type="text"
+                        placeholder="Escreva aqui o assunto da mentoria"
+                        className={styles.formStyle}
+                    />
+                </Form.Group>
             </section>
 
-            <section className={styles.userCardContainer}>
-                <Card style={{ width: '18rem' }} className={styles.userContainer}>
-                    <Card.Img variant="top" src={eduarda} className={styles.photo} alt="Eduarda é uma mulher negra, tem os cabelos cacheados, está num ambiente externo usando óculos escuros e sorrindo." />
-                    <Card.Title>Eduarda Mônica</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Desenvolvedora FullStack</Card.Subtitle>
-                    <Card.Title >Mentoria</Card.Title>
-                    <ListGroup className="list-group-flush" >
-                        <ListGroupItem className={styles.userContainer}>
-                            <p>
-                                30 Outubro 2021 <br />
-                                <strong>Data</strong>
-                            </p>
-                        </ListGroupItem>
-                        <ListGroupItem className={styles.userContainer}>
-                            <p>
-                                16:00 - 16:30 <br />
-                                <strong>Horário</strong>
-                            </p>
-                        </ListGroupItem>
-                        <ListGroupItem className={styles.userContainer}>
-                            <p>
-                                Teams <br />
-                                <strong>Onde?</strong>
-                            </p>
-                        </ListGroupItem>
-                    </ListGroup>
+            <section >
+                <Card className={styles.userCardContainer}>
+                    <Container className={styles.userContainer}>
+                        <Card.Img variant="top" src={eduarda} className={styles.photo} alt="Eduarda é uma mulher negra, tem os cabelos cacheados, está num ambiente externo usando óculos escuros e sorrindo." />
 
-                    <Button variant="outline-dark" className={styles.buttonStyle}>Marcar mentoria</Button>
+                        <Card.Title style={{ fontWeight: '700', fontSize: '1.3rem' }}>
+                            Eduarda Mônica
+                        </Card.Title>
+
+                        <Card.Text>
+                            eduarda@fcamara.com.br<br />
+                            Desenvolvedora FullStack
+                        </Card.Text>
+
+                        <Card.Title style={{ fontWeight: '700' }}>
+                            <hr />
+                            Resumo do agendamento
+                        </Card.Title>
+                    </Container>
+
+                    <Card className={styles.infoContainer}>
+                        <Card.Title style={{ fontWeight: '700' }}>
+                            Data
+                        </Card.Title>
+                        <Card.Text>
+                            30 Outubro 2021
+                        </Card.Text>
+
+                        <Card.Title style={{ fontWeight: '700' }}>
+                            Horário
+                        </Card.Title>
+                        <Card.Text>
+                            16:00 - 16:30
+                        </Card.Text>
+
+                        <Card.Title style={{ fontWeight: '700' }}>
+                            Onde?
+                        </Card.Title>
+                        <Card.Text>
+                            Teams
+                        </Card.Text>
+
+                        <Card.Title style={{ fontWeight: '700' }}>
+                            Assunto
+                        </Card.Title>
+                        <Card.Text>
+                            Gostaria de entender melhor como usar o Java.
+                        </Card.Text>
+
+                        <Button variant="outline-dark" className={styles.buttonStyle}>Marcar mentoria</Button>
+                    </Card>
                 </Card>
             </section>
-        </main>
+        </main >
     )
 }

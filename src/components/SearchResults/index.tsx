2f36228ec79eccf6bf8ba9f0d56e0a36a2a0a688
Aline Avelino <aline.savelino@gmail.com>
2022-04-15 00:50:09 -0300

    ajustes de estilização página de resultados

--- src/components/SearchResults/index.tsx
+++ src/components/SearchResults/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { SearchContext } from '../../context/SearchContext'
 import styles from './styles.module.scss'
 import error from '../../img/error.png'
+import abstractUser from '../../img/abstract-user.svg'
 
 type User = {
     profile: any
@@ -23,44 +24,38 @@ export function SearchResults() {
                 :
                 <>
                     <h2 className={styles.sectionName}>Sangues Laranjas que mais se encaixam na sua pesquisa:</h2>
-                    {searchResult.map((user: any, index: any) => {
-                        return (
-                            <>
-                                <Row lg={"auto"} md={2} xs={1} className={`g-4 ${styles.cardRow}`}>
-                                    <Col key={index}>
-                                        <Card style={{ width: '19.125rem', height: '30.188rem' }} className={styles.card} >
-                                            <Card.Img variant="top" className={`rounded-circle ${styles.photo}`} src={user.photo} alt={`Foto de perfil ${user.nickname}`} />
-                                            <Card.Body className={styles.bodyStyle}>
-                                                <Card.Title className={styles.name}>{user.nickname}</Card.Title>
+                    <Row lg={"auto"} md={2} xs={1} className={`g-4 ${styles.cardRow}`}>
+                        {searchResult.map((user: any, index: any) => {
+                            return (
+                                <Col key={index}>
+                                    <Card className={styles.card} >
+                                        <Card.Img variant="top" className={`rounded-circle ${styles.photo}`} src={user?.photo || abstractUser} alt={`Foto de perfil ${user.nickname}`} />
+                                        <Card.Body className={styles.bodyStyle}>
+                                            <Card.Title className={styles.name}>{user.nickname}</Card.Title>
 
-                                                <Card.Title className={styles.title}>Cargo</Card.Title>
-                                                <Card.Text className={styles.text}>
-                                                    {user.Role.name}
-                                                </Card.Text>
-                                                <Card.Title className={styles.title}>Senioridade</Card.Title>
-                                                <Card.Text className={styles.text}>
-                                                    {user.seniority}
-                                                </Card.Text>
-                                                <Card.Title className={styles.title}>Principais Skills</Card.Title>
-                                                <Card.Text className={styles.text}>
-                                                    {user.ProfileSkill.map((item: any) => {
-                                                        return item.skill.name.toUpperCase()
-                                                    }).join(' | ')}
-                                                </Card.Text>
-                                                <Link to={"/schedule"}><Button type="button" className={styles.button}>Agendar mentoria</Button></Link>
-
-                                            </Card.Body>
-                                        </Card>
-
-                                    </Col>
-                                </Row>
-                            </>
-                        )
-                    })}
+                                            <Card.Title className={styles.title}>Cargo</Card.Title>
+                                            <Card.Text className={styles.text}>
+                                                {user.Role.name}
+                                            </Card.Text>
+                                            <Card.Title className={styles.title}>Senioridade</Card.Title>
+                                            <Card.Text className={styles.text}>
+                                                {user.seniority}
+                                            </Card.Text>
+                                            <Card.Title className={styles.title}>Principais Skills</Card.Title>
+                                            <Card.Text className={styles.text}>
+                                                {user.ProfileSkill.map((item: any) => {
+                                                    return item.skill.name
+                                                }).join(' | ')}
+                                            </Card.Text>
+                                            <Link to={"/schedule"}><Button type="button" className={styles.button}>Agendar mentoria</Button></Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            )
+                        })}
+                    </Row>
                 </>
             }
-
-
         </section >
     )
 }
